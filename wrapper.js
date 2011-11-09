66a6b7d9033324cbce61a0ec01bbba86affd4041
Bence Danyi <madbence@gmail.com>
2011-11-09 21:28:17 +0100

    Proxy.js is depreached, use wrapper.js instead

---
+++ wrapper.js
@@ -0,0 +1,37 @@
+var net = require('net');
+var child = require('child_process');
+var fs = require('fs');
+
+	
+var server = net.createServer(function(c) {
+	var restart=function(err, fn)
+	{
+		if(instance != null)
+			instance.kill();
+		instance=child.fork(__dirname+'/hook2.js');
+		instance.on('message', function(m)
+		{
+			if(m.message != null)
+			{
+				f.write(m.message);
+				console.log('Child said: '+m.message);
+			}
+		});
+	}
+	var instance;
+	restart(null,null);
+	var watch=fs.watch(__dirname+'/hook2.js', restart);
+	var f=net.connect(6669, 'atw.irc.hu');
+	f.on('data', function(data)
+	{
+		console.log('srv: '+data);
+		instance.send({'message':data.toString()});
+		c.write(data);
+	});
+	c.on('data', function(data)
+	{
+		console.log('srv: '+data);
+		f.write(data);
+	});
+});
+server.listen(8124);
